6c1746642e65a5ccb2af7f5385a66d044a3eeaf7
edward_rog <anqiyuanms@hotmail.com>
2025-07-06 10:02:04 -0700

    add draw figures in parallel

--- src/dataset/make_dataset_video.py
+++ src/dataset/make_dataset_video.py
@@ -10,8 +10,11 @@ from glob import glob
 from pathlib import Path
 import numpy as np
 from scipy.interpolate import interp1d
+from tqdm import tqdm
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from moviepy import ImageSequenceClip
+from multiprocessing import Pool
 
 
 def find_matching_pairs(base_dir):
@@ -36,12 +39,18 @@ def find_matching_pairs(base_dir):
 
 def draw_2_by_2_images(csv_file='/home/edward/data/trav/all_image_depth_pair.csv'):
     """
-    1. read '/home/edward/data/trav/all_image_depth_pair.csv'
+    1. read csv_file
     2. draw a 2*2 figure: 1 img, 2 depth heatmap overlay img, 3 depth sector map
     3. save as images
     """
     # img_path, depth_path
     df = pd.read_csv(csv_file)
+    df['label'] = df['label'].apply(lambda x: os.path.join('/home/edward/data/trav/unlabeled_masks', os.path.basename(x)))
+    df.to_csv(csv_file, index=False)
+    exit()
+    colors = ['#00000000', 'lime']
+    cmap = ListedColormap(colors)
+    alpha = 0.6
     dpi = 200
     sector_left = -45 #-135
     sector_right = 45 # 135
@@ -51,10 +60,12 @@ def draw_2_by_2_images(csv_file='/home/edward/data/trav/all_image_depth_pair.csv
     angle_rad_max = np.deg2rad(angle_max)
     min_pct = (angle_min+45)/90  # percentile for cropping
     max_pct = (angle_max+45)/90
-    for i, row in df.iterrows():
+    
+    for i, row in tqdm(df.iterrows(), total=len(df)):
         img_basename = os.path.splitext(os.path.basename(row['image']))[0]
         laser_basename = os.path.splitext(os.path.basename(row['depth']))[0]
         rgb_image = plt.imread(row['image'])
+        mask = np.load(row['label'])
 
         with open(row['depth'], 'rb') as f:
             data = pickle.load(f)
@@ -67,15 +78,21 @@ def draw_2_by_2_images(csv_file='/home/edward/data/trav/all_image_depth_pair.csv
             axes[0,0].figure.dpi = dpi
             axes[0,0].axis('off')
 
-            axes[1,0] = plt.subplot(223, projection='polar')
-            axes[1,0].plot(angles, vector)
-            axes[1,0].plot([angle_rad_max, angle_rad_max], [0, 5.1], color='red', linestyle='--')
-            axes[1,0].plot([angle_rad_min, angle_rad_min], [0, 5.1], color='blue', linestyle='--')
-            axes[1,0].set_thetamin(sector_left)
-            axes[1,0].set_thetamax(sector_right)
-            axes[1,0].set_theta_zero_location('N')
-            axes[1,0].set_xticks(np.pi/180. * np.linspace(sector_left, sector_right, 10, endpoint=False))
-            axes[1,0].figure.axes[2].set_axis_off()
+            axes[1,0].imshow(rgb_image)
+            axes[1,0].imshow(mask, cmap=cmap, alpha=alpha)
+            axes[1,0].figure.dpi = dpi
+            axes[1,0].axis('off')
+
+            axes[1,1] = plt.subplot(224, projection='polar')
+            axes[1,1].plot(angles, vector)
+            axes[1,1].plot([angle_rad_max, angle_rad_max], [0, 5.1], color='red', linestyle='--')
+            axes[1,1].plot([angle_rad_min, angle_rad_min], [0, 5.1], color='blue', linestyle='--')
+            axes[1,1].set_thetamin(sector_left)
+            axes[1,1].set_thetamax(sector_right)
+            axes[1,1].set_theta_zero_location('N')
+            axes[1,1].set_xticks(np.pi/180. * np.linspace(sector_left, sector_right, 10, endpoint=False))
+            axes[1,1].figure.axes[3].set_axis_off()
+            # axes[1,1].set_frame_on(False)
 
             depth_vector = vector[int(min_pct* len(vector)):int(max_pct*len(vector))][::-1]
             x_original = np.linspace(0, 1, len(depth_vector)) # x-coordinates for original data
@@ -89,11 +106,85 @@ def draw_2_by_2_images(csv_file='/home/edward/data/trav/all_image_depth_pair.csv
             axes[0,1].axis('off')
             idx = f'{img_basename}_{laser_basename}'
 
-            axes[1,1].axis('off')
-
             plt.subplots_adjust(hspace=0.01, wspace=0.01)
-            plt.savefig(f"output/labeled/{idx}.png",bbox_inches='tight', pad_inches=0.01, dpi=dpi)
+            plt.savefig(f"output/unlabeled/{idx}.png",bbox_inches='tight', pad_inches=0.01, dpi=dpi)
             plt.close(fig)
+
+
+def process_chunk(df_chunk):
+    colors = ['#00000000', 'lime']
+    cmap = ListedColormap(colors)
+    alpha = 0.6
+    dpi = 200
+    sector_left = -45
+    sector_right = 45
+    angle_min = -26
+    angle_max = 36
+    angle_rad_min = np.deg2rad(angle_min)
+    angle_rad_max = np.deg2rad(angle_max)
+    min_pct = (angle_min + 45) / 90
+    max_pct = (angle_max + 45) / 90
+
+    for _, row in tqdm(df_chunk.iterrows(), total=len(df_chunk)):
+        img_basename = os.path.splitext(os.path.basename(row['image']))[0]
+        laser_basename = os.path.splitext(os.path.basename(row['depth']))[0]
+        rgb_image = plt.imread(row['image'])
+        mask = np.load(row['label'])
+
+        with open(row['depth'], 'rb') as f:
+            data = pickle.load(f)
+
+            vector = np.array(data['ranges'][::-1])[540:900]
+            angles = np.linspace(np.deg2rad(sector_left), np.deg2rad(sector_right), len(vector), endpoint=False)
+
+            fig, axes = plt.subplots(2, 2)
+            axes[0,0].imshow(rgb_image)
+            axes[0,0].figure.dpi = dpi
+            axes[0,0].axis('off')
+
+            axes[1,0].imshow(rgb_image)
+            axes[1,0].imshow(mask, cmap=cmap, alpha=alpha)
+            axes[1,0].figure.dpi = dpi
+            axes[1,0].axis('off')
+
+            # Remove the default axes[1,1] and replace with polar subplot
+            fig.delaxes(axes[1,1])
+            polar_ax = fig.add_subplot(2, 2, 4, projection='polar')
+            polar_ax.plot(angles, vector)
+            polar_ax.plot([angle_rad_max, angle_rad_max], [0, 5.1], color='red', linestyle='--')
+            polar_ax.plot([angle_rad_min, angle_rad_min], [0, 5.1], color='blue', linestyle='--')
+            polar_ax.set_thetamin(sector_left)
+            polar_ax.set_thetamax(sector_right)
+            polar_ax.set_theta_zero_location('N')
+            polar_ax.set_xticks(np.pi/180. * np.linspace(sector_left, sector_right, 10, endpoint=False))
+            # Hide polar spine rectangle but keep the polar grid and ticks
+            for spine in polar_ax.spines.values():
+                spine.set_visible(False)
+
+            depth_vector = vector[int(min_pct * len(vector)):int(max_pct * len(vector))][::-1]
+            x_original = np.linspace(0, 1, len(depth_vector))
+            f_interp = interp1d(x_original, depth_vector)
+            x_new = np.linspace(0, 1, rgb_image.shape[1])
+            interp_vector = f_interp(x_new)
+            tiled_vector = np.tile(interp_vector, (rgb_image.shape[0], 1))
+
+            axes[0,1].imshow(rgb_image)
+            axes[0,1].imshow(tiled_vector, cmap='autumn', vmin=0, vmax=5, alpha=0.45)
+            axes[0,1].figure.dpi = dpi
+            axes[0,1].axis('off')
+
+            idx = f'{img_basename}_{laser_basename}'
+            # os.makedirs("output/unlabeled", exist_ok=True)
+            plt.subplots_adjust(hspace=0.01, wspace=0.01)
+            plt.savefig(f"output/unlabeled/{idx}.png", bbox_inches='tight', pad_inches=0.01, dpi=dpi)
+            plt.close(fig)
+
+
+def draw_2_by_2_images_parallel(csv_file, num_parts=4):
+    df = pd.read_csv(csv_file)
+    df_chunks = np.array_split(df, num_parts)
+    with Pool(num_parts) as pool:
+        pool.map(process_chunk, df_chunks)
 
 
 def make_video(image_dir="output/depth"):
@@ -103,7 +194,7 @@ def make_video(image_dir="output/depth"):
     image_files = sorted(glob(f"{image_dir}/*.png"))
 
     clip = ImageSequenceClip(image_files, fps=24)
-    clip.write_videofile("output/labeled/output_video.mp4", codec="libx264")
+    clip.write_videofile("output/unlabeled_dataset.mp4", codec="libx264")
 
 
 def make_labeled_dataset():
@@ -183,9 +274,10 @@ def split_unlabeled_dataset():
 
 if __name__ == '__main__':
     # save_all_image_depth_pairs()
-    # draw_2_by_2_images("/home/edward/data/segmentation_indoor_images/labeled_rgbd_pairs.csv")
-    # make_video("output/labeled")
+    # draw_2_by_2_images("/home/edward/data/trav/unlabeled_masks.csv")
+    make_video("output/unlabeled")
     # make_labeled_dataset()
     # append_depth_to_labeled_csv()
     # check_labeled_pairs()
-    split_unlabeled_dataset()
+    # split_unlabeled_dataset()
+    # draw_2_by_2_images_parallel("/home/edward/data/trav/unlabeled_masks.csv", num_parts=8)
